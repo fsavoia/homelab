88abba3b3f2f84ab46a7da315b1c9624ba754b48
Felipe Savoia <fmsavoia@gmail.com>
2025-02-02 21:27:07 +0100

    remove output

--- terraform/oracle/infra_backend/output.tf
+++
@@ -1,9 +0,0 @@
-output "bucket_name" {
-  description = "Bucket Name"
-  value       = oci_objectstorage_bucket.infra_backend_bucket.name
-}
-
-output "bucket_id" {
-  description = "Bucket OCID"
-  value       = oci_objectstorage_bucket.infra_backend_bucket.id
-}
