f8c2bc1002d5bbb651d192190419c75d2054202f
Felipe Savoia <fmsavoia@gmail.com>
2025-02-05 23:06:58 +0100

    add ansible k3s automation

--- infrastructure/oracle/kubernetes/public-security-list.tf
+++ infrastructure/oracle/kubernetes/public-security-list.tf
@@ -1,3 +1,13 @@
+data "oci_core_vnic_attachments" "worker_vnics" {
+  compartment_id = var.compartment_id
+  instance_id    = oci_core_instance.ubuntu_arm_instance_worker.id
+}
+
+data "oci_core_vnic" "worker_vnic_details" {
+  count   = length(data.oci_core_vnic_attachments.worker_vnics.vnic_attachments)
+  vnic_id = data.oci_core_vnic_attachments.worker_vnics.vnic_attachments[count.index].vnic_id
+}
+
 resource "oci_core_security_list" "public-security-list" {
   compartment_id = var.compartment_id
   vcn_id         = module.vcn.vcn_id
@@ -35,6 +45,13 @@ resource "oci_core_security_list" "public-security-list" {
       min = 22
       max = 22
     }
+  }
+
+  ingress_security_rules {
+    stateless   = false
+    source      = data.oci_core_vnic.worker_vnic_details[*].public_ip
+    source_type = "CIDR_BLOCK"
+    protocol    = "all"
   }
 
   ingress_security_rules {
