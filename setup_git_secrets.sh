08121ee5184a20e9dcc4a6e396c0773462449486
Felipe Savoia <fmsavoia@gmail.com>
2025-02-05 17:00:33 +0100

    Create script to create the git-secret setup

---
+++ setup_git_secrets.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Function to print colored messages
+print_color() {
+    case $1 in
+        "green") color="\033[0;32m" ;;
+        "yellow") color="\033[0;33m" ;;
+        "red") color="\033[0;31m" ;;
+        *) color="\033[0m" ;;
+    esac
+    echo -e "${color}$2\033[0m"
+}
+
+# Install git-secrets
+if ! command -v git-secrets &> /dev/null
+then
+    print_color "yellow" "git-secrets not found, installing..."
+    if [ "$(uname)" = "Darwin" ]; then
+        brew install git-secrets
+    elif [ "$(uname)" = "Linux" ]; then
+        sudo apt-get update
+        sudo apt-get install -y git-secret
+    fi
+else
+    print_color "green" "git-secrets is already installed."
+fi
+
+# Configure git-secrets
+print_color "yellow" "Configuring git-secrets..."
+git secrets --install
+git secrets --register-aws
+git secrets --install -f
+
+print_color "green" "git-secrets configuration completed!"
